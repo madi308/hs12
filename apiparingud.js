c0946d6f4850c95c47835f02fb64db4e194997be
madi308 <77383233+madi308@users.noreply.github.com>
2024-05-28 11:15:56 +0300

    Update apiparingud.js

--- apiparingud.js
+++ apiparingud.js
@@ -99,13 +99,13 @@ async function handleFormSubmit(event) {
 
 async function listiraamatud() {
 
-    const responseData = await getDataAsJson("http://localhost:5000/raamatud/");
+    const responseData = await getDataAsJson("https://hs9-flask-api-raamatud.azurewebsites.net/raamatud/");
     const resultElement = document.getElementById("raamatud_result");
     resultElement.innerHTML = "";
     for (var raamat of responseData.raamatud) {
         raamat = raamat.split(".")[0];
-        resultElement.innerHTML += '<a href="http://localhost:5000/raamatud/'+raamat+'"  download="'+raamat+'.txt" >' +raamat+".txt</a> " +
-			'<a href="#" onclick="deleteObject(\'http://localhost:5000/raamatud/'+raamat+'\')" > [kustuta]</a>' +
+        resultElement.innerHTML += '<a href="https://hs9-flask-api-raamatud.azurewebsites.net/raamatud/'+raamat+'"  download="'+raamat+'.txt" >' +raamat+".txt</a> " +
+			'<a href="#" onclick="deleteObject(\'https://hs9-flask-api-raamatud.azurewebsites.net/raamatud/'+raamat+'\')" > [kustuta]</a>' +
 			"<br />";
     }
 }
